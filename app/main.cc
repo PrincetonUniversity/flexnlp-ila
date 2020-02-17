5646ce0516200b1dc830cb34a30bac45b3a38829
Yi Li <yi@dynamic-oit-eenet-c-24.princeton.edu>
2020-02-17 16:36:57 -0500

    merge

--- app/main.cc
+++ app/main.cc
@@ -74,57 +74,58 @@ int main(int argc, char *argv[]) {
   return 0;
 }
 
-void IlaSim::sim_gen_decode_d() {
-  std::queue<InstrLvlAbsPtr> ila_queue;
-  ila_queue.push(model_ptr_);
-  DebugLog::Enable("ILA hierarchy");
-  while (!ila_queue.empty()) {
-    auto current_ila = ila_queue.front();
-    ila_queue.pop();
-    ILA_INFO << "current_ila_name:" << current_ila->name();
-    for (unsigned int i = 0; i < current_ila->child_num(); i++) {
-      ILA_INFO << "current_ila_child:" << current_ila->child(i)->name();
-      ila_queue.push(current_ila->child(i));
-    }
-    for (unsigned int i = 0; i < current_ila->instr_num(); i++) {
-      ILA_INFO << "current_ila_instr:" << current_ila->instr(i)->name();
-      create_decode_d(current_ila->instr(i));
-    }
-    ILA_INFO << std::endl;
-  }
-  DebugLog::Disable("ILA hierarchy");
-}
+// these two functions are used for debugging only.
+// void IlaSim::sim_gen_decode_d() {
+//   std::queue<InstrLvlAbsPtr> ila_queue;
+//   ila_queue.push(model_ptr_);
+//   DebugLog::Enable("ILA hierarchy");
+//   while (!ila_queue.empty()) {
+//     auto current_ila = ila_queue.front();
+//     ila_queue.pop();
+//     ILA_INFO << "current_ila_name:" << current_ila->name();
+//     for (unsigned int i = 0; i < current_ila->child_num(); i++) {
+//       ILA_INFO << "current_ila_child:" << current_ila->child(i)->name();
+//       ila_queue.push(current_ila->child(i));
+//     }
+//     for (unsigned int i = 0; i < current_ila->instr_num(); i++) {
+//       ILA_INFO << "current_ila_instr:" << current_ila->instr(i)->name();
+//       create_decode_d(current_ila->instr(i));
+//     }
+//     ILA_INFO << std::endl;
+//   }
+//   DebugLog::Disable("ILA hierarchy");
+// }
 
-void IlaSim::create_decode_d(const InstrPtr& instr_expr) {
-  std::stringstream decode_function;
-  std::string indent = "";
-  std::string decode_func_name;
-  ILA_INFO << "cd_0";
-  if (readable_)
-    decode_func_name = "decode_" + instr_expr->host()->name().str() + "_" +
-                       instr_expr->name().str();
-  else
-    decode_func_name =
-        "decode_" + std::to_string(instr_expr->decode()->name().id());
-  ILA_INFO << "cd_1";
-  auto decode_expr = instr_expr->decode();
-  decode_decl(decode_function, indent, decode_func_name);
-  auto valid_expr = instr_expr->host()->valid();
-  auto DfsKernel = [this, &decode_function, &indent](const ExprPtr& e) {
-    dfs_kernel(decode_function, indent, e);
-  };
+// void IlaSim::create_decode_d(const InstrPtr& instr_expr) {
+//   std::stringstream decode_function;
+//   std::string indent = "";
+//   std::string decode_func_name;
+//   ILA_INFO << "cd_0";
+//   if (readable_)
+//     decode_func_name = "decode_" + instr_expr->host()->name().str() + "_" +
+//                        instr_expr->name().str();
+//   else
+//     decode_func_name =
+//         "decode_" + std::to_string(instr_expr->decode()->name().id());
+//   ILA_INFO << "cd_1";
+//   auto decode_expr = instr_expr->decode();
+//   decode_decl(decode_function, indent, decode_func_name);
+//   auto valid_expr = instr_expr->host()->valid();
+//   auto DfsKernel = [this, &decode_function, &indent](const ExprPtr& e) {
+//     dfs_kernel(decode_function, indent, e);
+//   };
 
-  valid_expr->DepthFirstVisit(DfsKernel);
-  decode_check_valid(decode_function, indent, valid_expr, instr_expr);
-  ILA_INFO << "cd2";
-  decode_expr->DepthFirstVisit(DfsKernel);
-  ILA_INFO << "cd3";
-  decode_return(decode_function, indent, decode_expr, instr_expr);
-  ILA_INFO << "cd4";
-  decrease_indent(indent);
-  decode_function << indent << "};" << std::endl;
+//   valid_expr->DepthFirstVisit(DfsKernel);
+//   decode_check_valid(decode_function, indent, valid_expr, instr_expr);
+//   ILA_INFO << "cd2";
+//   decode_expr->DepthFirstVisit(DfsKernel);
+//   ILA_INFO << "cd3";
+//   decode_return(decode_function, indent, decode_expr, instr_expr);
+//   ILA_INFO << "cd4";
+//   decrease_indent(indent);
+//   decode_function << indent << "};" << std::endl;
 
-  decode_export(decode_function, decode_func_name);
-  decode_mk_file(decode_func_name);
-  return;
-}
+//   decode_export(decode_function, decode_func_name);
+//   decode_mk_file(decode_func_name);
+//   return;
+// }
