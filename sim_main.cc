a507ccda39dee4f074251a26e0ae0e21f6443f14
Yi Li <yi@dynamic-oit-eenet-c-24.princeton.edu>
2020-02-24 20:38:39 -0500

    testbench update

--- sim_main.cc
+++ sim_main.cc
@@ -10,10 +10,10 @@ SC_MODULE(Source) {
   sc_in<bool> clk{"clk"};
   //sc_in<bool> rst;
   
-  sc_out<sc_biguint<1>> flex_wr_in;
-  sc_out<sc_biguint<1>> flex_rd_in;
-  sc_out<sc_biguint<32>> flex_addr_in;
-  sc_out<sc_biguint<8>> flex_data_in[16];
+  sc_out< sc_biguint<1> > flex_wr_in;
+  sc_out< sc_biguint<1> > flex_rd_in;
+  sc_out< sc_biguint<32> > flex_addr_in;
+  sc_out< sc_biguint<8> > flex_data_in[16];
 
 //  void source_input();
 
@@ -77,56 +77,69 @@ SC_MODULE(Source) {
 };
 
 SC_MODULE(testbench) {
-  flex_sim *flex;
-  Source *src;
+  SC_HAS_PROCESS(testbench);
+  flex_sim flex;
+  Source src;
 
-  sc_signal<sc_biguint<1>> flex_wr_signal;
-  sc_signal<sc_biguint<1>> flex_rd_signal;
-  sc_signal<sc_biguint<32>> flex_addr_signal;
-  sc_signal<sc_biguint<8>> flex_data_signal[16];
+  sc_clock clk;
+  sc_signal< sc_biguint<1> > flex_wr_signal;
+  sc_signal< sc_biguint<1> > flex_rd_signal;
+  sc_signal< sc_biguint<32> > flex_addr_signal;
+  sc_signal< sc_biguint<8> > flex_data_signal[16];
 
-  sc_clock clk_sig;
-
-  SC_CTOR(testbench): clk_sig ("clk", 1, SC_NS) {
-    flex = new flex_sim("flex_sim");
-    src = new Source("src");
-
-    // binding source ports to wires
-    src->clk(clk_sig);
-    src->flex_wr_in(flex_wr_signal);
-    src->flex_rd_in(flex_rd_signal);
-    src->flex_addr_in(flex_addr_signal);
+  testbench(sc_module_name name)
+  : sc_module(name),
+    clk("clk", 1, SC_NS),
+    src("source"),
+    flex("flexnlp")
+  {
+    // binding the signals from the source
+    src.clk(clk);
+    src.flex_wr_in(flex_wr_signal);
+    src.flex_rd_in(flex_rd_signal);
+    src.flex_addr_in(flex_addr_signal);
     for (int i = 0; i < 16; i++) {
-      src->flex_data_in[i](flex_data_signal[i]);
+      src.flex_data_in[i](flex_data_signal[i]);
     }
 
-    // binding wires to FlexNLP model
-    flex->flex_sim_if_axi_rd_in(flex_rd_signal);
-    flex->flex_sim_if_axi_wr_in(flex_wr_signal);
-    flex->flex_sim_addr_in_in(flex_addr_signal);
-    flex->flex_sim_data_in_0_in(flex_data_signal[0]);
-    flex->flex_sim_data_in_1_in(flex_data_signal[1]);
-    flex->flex_sim_data_in_2_in(flex_data_signal[2]);
-    flex->flex_sim_data_in_3_in(flex_data_signal[3]);
-    flex->flex_sim_data_in_4_in(flex_data_signal[4]);
-    flex->flex_sim_data_in_5_in(flex_data_signal[5]);
-    flex->flex_sim_data_in_6_in(flex_data_signal[6]);
-    flex->flex_sim_data_in_7_in(flex_data_signal[7]);
-    flex->flex_sim_data_in_8_in(flex_data_signal[8]);
-    flex->flex_sim_data_in_9_in(flex_data_signal[9]);
-    flex->flex_sim_data_in_10_in(flex_data_signal[10]);
-    flex->flex_sim_data_in_11_in(flex_data_signal[11]);
-    flex->flex_sim_data_in_12_in(flex_data_signal[12]);
-    flex->flex_sim_data_in_13_in(flex_data_signal[13]);
-    flex->flex_sim_data_in_14_in(flex_data_signal[14]);
-    flex->flex_sim_data_in_15_in(flex_data_signal[15]);
+    // binding the signals for the model
+    flex.flex_sim_if_axi_rd_in(flex_rd_signal);
+    flex.flex_sim_if_axi_wr_in(flex_wr_signal);
+    flex.flex_sim_addr_in_in(flex_addr_signal);
+    flex.flex_sim_data_in_0_in(flex_data_signal[0]);
+    flex.flex_sim_data_in_1_in(flex_data_signal[1]);
+    flex.flex_sim_data_in_2_in(flex_data_signal[2]);
+    flex.flex_sim_data_in_3_in(flex_data_signal[3]);
+    flex.flex_sim_data_in_4_in(flex_data_signal[4]);
+    flex.flex_sim_data_in_5_in(flex_data_signal[5]);
+    flex.flex_sim_data_in_6_in(flex_data_signal[6]);
+    flex.flex_sim_data_in_7_in(flex_data_signal[7]);
+    flex.flex_sim_data_in_8_in(flex_data_signal[8]);
+    flex.flex_sim_data_in_9_in(flex_data_signal[9]);
+    flex.flex_sim_data_in_10_in(flex_data_signal[10]);
+    flex.flex_sim_data_in_11_in(flex_data_signal[11]);
+    flex.flex_sim_data_in_12_in(flex_data_signal[12]);
+    flex.flex_sim_data_in_13_in(flex_data_signal[13]);
+    flex.flex_sim_data_in_14_in(flex_data_signal[14]);
+    flex.flex_sim_data_in_15_in(flex_data_signal[15]);
+
+    SC_THREAD(run);
   }
 
-  ~testbench() {
-    delete flex;
-    delete src;
+  void run() {
+    wait(2, SC_NS);
+    std::cout << "@" << sc_time_stamp() << "simulation start" << std::endl;
+    wait(8, SC_NS);
+    std::cout << "@" << sc_time_stamp() << "start input" << std::endl;
+    wait(100000, SC_NS);
+    std::cout << "@" << sc_time_stamp() << "sc_stop" << std::endl;
+    sc_stop();
   }
   
+  ~testbench() {
+    delete &flex;
+    delete &src;
+  }
 };
 
 int sc_main(int argc, char *argv[]) {
