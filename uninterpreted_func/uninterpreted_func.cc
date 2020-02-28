4b7471624c641fb0fb09aeced59a097afb71a670
Yi Li <yi@dynamic-oit-eenet-c-24.princeton.edu>
2020-02-28 13:16:26 -0500

    uninterpreted functions scope added

--- uninterpreted_func/uninterpreted_func.cc
+++ uninterpreted_func/uninterpreted_func.cc
@@ -1,16 +1,16 @@
 #include "systemc.h"
 #include "flex_sim.h"
 
-sc_biguint<8> signed_gt(sc_biguint<8> arg_0, sc_biguint<8> arg_1) {
-  int arg0_int = arg0.to_int();
-  int arg1_int = arg1.to_int();
+sc_biguint<8> flex_sim::signed_gt(sc_biguint<8> arg_0, sc_biguint<8> arg_1) {
+  int arg_0_int = arg_0.to_int();
+  int arg_1_int = arg_1.to_int();
 
-  int sb_0 = arg0_int >> 7;
-  int sb_1 = arg1_int >> 7;
+  int sb_0 = arg_0_int >> 7;
+  int sb_1 = arg_1_int >> 7;
 
-  unsigned int s_arg0 = (sb_0) ? (~arg0_int + 1) : arg0_int;
-  unsigned int s_arg1 = (sb_1) ? (~arg1_int + 1) : arg1_int;
+  unsigned int s_arg_0 = (sb_0) ? (~arg_0_int + 1) : arg_0_int;
+  unsigned int s_arg_1 = (sb_1) ? (~arg_1_int + 1) : arg_1_int;
   
-  sc_biguint<8> result = (s_arg0 > s_arg1)? arg0 : arg1;
+  sc_biguint<8> result = (s_arg_0 > s_arg_1)? arg_0 : arg_1;
   return result;
 }
