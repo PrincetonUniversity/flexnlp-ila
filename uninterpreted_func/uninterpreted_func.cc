83a2b83409d9988d09e8a7a555208c07b6b14604
Yi Li <yl29@princeton.edu>
2020-03-03 19:56:10 -0500

    uninterpreted function update

--- uninterpreted_func/uninterpreted_func.cc
+++ uninterpreted_func/uninterpreted_func.cc
@@ -8,8 +8,8 @@ sc_biguint<8> flex_sim::signed_gt(sc_biguint<8> arg_0, sc_biguint<8> arg_1) {
   int sb_0 = arg_0_int >> 7;
   int sb_1 = arg_1_int >> 7;
 
-  int s_arg_0 = (sb_0 > 0) ? (arg_0_int - 256) : arg_0_int;
-  int s_arg_1 = (sb_1 > 0) ? (arg_1_int - 256) : arg_1_int;
+  int s_arg_0 = ((sb_0 > 0) ? (~arg_0_int + 1) : arg_0_int) & 127;
+  int s_arg_1 = ((sb_1 > 0) ? (~arg_1_int + 1) : arg_1_int) & 127;
   
   sc_biguint<8> result = (s_arg_0 > s_arg_1)? arg_0 : arg_1;
   return result;
