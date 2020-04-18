93800ed76ddf1451ae92a3f4f924f369bd7d19ec
Yi Li <yl29@princeton.edu>
2020-04-17 23:41:53 -0400

    placeholder for uninterpreted functions added

--- uninterpreted_func/uninterpreted_func.cc
+++ uninterpreted_func/uninterpreted_func.cc
@@ -23,3 +23,55 @@ sc_biguint<8> flex_sim::signed_gt(sc_biguint<8> arg_0, sc_biguint<8> arg_1) {
   
   return result;
 }
+
+sc_biguint<20> flex_sim::PEActEadd(sc_biguint<20> arg_0, sc_biguint<20> arg_1) {
+  // TODO: need to implement signed Add
+  return arg_0;
+}
+
+sc_biguint<20> PEActEmul(sc_biguint<20> arg_0, sc_biguint<20> arg_1) {
+  // TODO: implement signed multiply
+  return arg_0;
+}
+
+sc_biguint<20> PEActSigmoid(sc_biguint<20> arg_0) {
+  // TODO: implement Sigmoid
+  return arg_0;
+}
+
+sc_biguint<20> PEActTanh(sc_biguint<20> arg_0) {
+  // TODO: implement Tanh
+  return arg_0;
+}
+
+sc_biguint<20> PEActRelu(sc_biguint<20> arg_0) {
+  // TODO: implement Relu
+  return arg_0;
+}
+
+sc_biguint<20> PEActOnex(sc_biguint<20> arg_0) {
+  // TODO: implement Onex
+  return arg_0;
+}
+
+sc_biguint<20> Adptflow2Fixed(sc_biguint<8> arg_0, sc_biguint<3> arg_1) {
+  // TODO: implement adptflow to fixed
+  sc_biguint<20> result = 0xabcd;
+  
+  return result;
+}
+
+sc_biguint<8> Fixed2Adaptflow(sc_biguint<20> arg_0, sc_biguint<3> arg_1) {
+  // TODO: implement fixed to adaptflow
+  sc_biguint<8> result = 0xab;
+
+  return result;
+}
+
+
+
+
+
+
+
+
